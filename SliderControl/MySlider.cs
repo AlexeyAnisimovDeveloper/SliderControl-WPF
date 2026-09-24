26eb97d15e40b2910735c9d05f7a6f78e11da45c
Aleksey Anisimov <aleksey.developer@gmail.com>
2026-09-24 18:20:58 +0300

    Add project files.

---
+++ SliderControl/MySlider.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MySliderControl;
+
+public class MySlider : Slider
+{
+    static MySlider()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(MySlider), new FrameworkPropertyMetadata(typeof(MySlider)));
+    }
+}
